0d53b9ec154302836b7830e1b3fffa132c51c75e
Alexandre Doneux <he201962@students.ephec.be>
2022-11-17 12:16:33 +0100

    Nouveau Repository pour repartir sur une organisation des fichiers.

---
+++ main/main.ino
@@ -0,0 +1,70 @@
+
+#include <SoftwareSerial.h>
+
+#define rxPin 11 // Broche 11 en tant que RX, à raccorder sur TX du HC-05
+#define txPin 10 // Broche 10 en tant que TX, à raccorder sur RX du HC-05 
+SoftwareSerial hc05(rxPin, txPin);
+
+#define ledTest 5
+
+
+
+
+void setup()
+{
+Serial.begin(9600); // intitialise la connexion série à travers l'alimentation
+
+//Serial.begin(38400); 
+//Serial.begin(74880); 
+
+hc05.begin(9600); // intitialise la connexion série à travers le hc05
+
+pinMode(A3,INPUT);
+pinMode(ledTest,OUTPUT);
+
+digitalWrite(ledTest, LOW);
+
+
+}
+
+
+void loop() {
+  Serial.print(0);
+Serial.print(" ");
+Serial.print(1000);
+Serial.print(" ");
+
+  // Mis en commentaire suite au test 
+  
+  int readValue = analogRead(A3);
+
+  Serial.println(readValue);
+  //delay(5);
+
+  if(readValue>310){
+    digitalWrite(ledTest, HIGH);
+    
+  }
+  else{
+    digitalWrite(ledTest, LOW);
+
+  }
+  
+
+  
+
+  if(hc05.available()){
+    int givenNumber = hc05.read();
+    Serial.write(givenNumber);
+
+    if(givenNumber > 10){
+      digitalWrite(ledTest, HIGH);
+    }
+    else{
+      digitalWrite(ledTest, LOW);
+    }
+  }
+  
+
+  
+}
