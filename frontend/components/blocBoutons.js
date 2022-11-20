fd504b38532221a0306a5170e7d344944d3f6b41
Alexandre Doneux <he201962@students.ephec.be>
2022-11-20 19:31:18 +0100

    [FEAT] Ajout d'une fonction de vérification de si le bluetooth est activé sur le téléphone.
    Au tout début on fera une vérification de si le bluetooth est activé sur le téléphone pour avertir l'utilisateur si ce n'est pas
    le cas.

--- frontend/components/blocBoutons.js
+++ frontend/components/blocBoutons.js
@@ -8,10 +8,12 @@ class BlocBoutons extends Component{
     constructor(props){
         super(props);
         this.state={
+            bluetoothEnabled : false,
             device : "coucou",
 
         }
         this.changeUpperState=this.changeUpperState.bind(this);
+        this.checkBluetoothEnabled=this.checkBluetoothEnabled.bind(this);
         this.alertState=this.alertState.bind(this);
     }
 
@@ -20,6 +22,19 @@ class BlocBoutons extends Component{
         //this.setState({[whatToChange] : value});
 
     }
+
+    async checkBluetoothEnabled() {
+        try {
+          console.log('App::componentDidMount Checking bluetooth status');
+          let enabled = await RNBluetoothClassic.isBluetoothEnabled();
+    
+          console.log(`App::componentDidMount Status: ${enabled}`);
+          this.setState({ bluetoothEnabled: enabled });
+        } catch (error) {
+          console.log('App::componentDidMount Status Error: ', error);
+          this.setState({ bluetoothEnabled: false });
+        }
+      }
 
     alertState(){
         alert(this.state.device.name);
