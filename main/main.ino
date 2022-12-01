e003a40cfb662f4bbe9dd02a1e6c9852f2f3cbda
Alexandre Doneux <he201962@students.ephec.be>
2022-12-01 18:19:22 +0100

    [FIX] Correction des problème de données corrompues en utilisant plus softwareSerial.
    Quand j'utilisait softwareSerial pour la communication Bluetooth j'avais des problèmes pour lire les données envoyées au bracelet.
    Après plusieurs tests j'ai remarqué que c'était le cas lorsque la lecture était dans un if qui vérifiais avec available() qu'il y
    avait des données disponibles. Lire à chaque loop ralentit énormément le fonctionnement vu que le read prends du temps (100 ms).
    
    Ce commit est en lien avec la US 3.2, issue #4

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,10 @@
+// Dans ce fichier on transvase puis on connecte le module bluetooth aux TX et RX du l'arduino. Serial sera ces pins (0 et 1)
+// On envoie par le odule et pour vérifier que ça marche bien on réenvoie un println par là
 
 
-#include <SoftwareSerial.h>
+ // Broche 11 en tant que RX, à raccorder sur TX du HC-05
+ // Broche 10 en tant que TX, à raccorder sur RX du HC-05 
 
-#define rxPin 11 // Broche 11 en tant que RX, à raccorder sur TX du HC-05
-#define txPin 10 // Broche 10 en tant que TX, à raccorder sur RX du HC-05 
-SoftwareSerial hc05(rxPin, txPin);
 
 #define ledTest 5
 #define ledReceive 4
@@ -15,9 +15,9 @@ void setup()
 {
 Serial.begin(9600); // intitialise la connexion série à travers l'alimentation
 
+Serial.setTimeout(100);
 
-hc05.begin(9600); // intitialise la connexion série à travers le hc05
-hc05.setTimeout(100); // set new value to 100 milliseconds
+
 
 pinMode(A3,INPUT);
 pinMode(ledTest,OUTPUT);
@@ -37,75 +37,25 @@ void loop() {
   
   int readValue = analogRead(A3);
 
-  //Serial.println(readValue);
+  //Serial.println(readValue);  //remettre
   
-  hc05.println(readValue);  
 
 
   if(readValue>310){
-    Serial.println("Au dessus\n");
-    digitalWrite(ledTest, HIGH);
-    //hc05.println(readValue);   
+    //Serial.print("Au dessus\n"); 
+    digitalWrite(ledTest, HIGH);  
   }
   else{
     digitalWrite(ledTest, LOW);
 
   }
 
-/* En dehors du if ça marche
-   String incomingString = hc05.readStringUntil('\n');
 
-
-    // prints the received data
-    Serial.print("I received: ");
-    Serial.println(incomingString);
-    delay(100);
-  
- */
-
-/*
-// dans le if on a des problèmes de lecture. On obtiens pas la donnée correcte
-  if (hc05.available() > 0) {
-    //String incomingString = Serial.readStringUntil('\n');
-    String incomingString = hc05.readStringUntil('\n');
-
-
-    // prints the received data
-    Serial.print("I received: ");
-    Serial.println(incomingString);
-    delay(100);
-  } 
-
-*/
-
-
-  if(hc05.available()){
+  if(Serial.available() > 0){
     digitalWrite(ledReceive, HIGH);
-
-    
-
-    String receivedData = hc05.readStringUntil("\n");
-    //String receivedData = Serial.readStringUntil("\n");
-
-
-    receivedData.trim(); // Enlève \r, \n ou des espaces à la fin du string si il y en a. Normalement pas besoin car readStringUntil ne stocke pas le délimiteur
-
-
+    String receivedData = Serial.readStringUntil("\n");
     Serial.println(receivedData);
-
-
-    if(receivedData == "coucou"){
-      digitalWrite(ledReceive, HIGH);
-    }
-    
     digitalWrite(ledReceive, LOW);
   }
-
-
-  
-
-
-
-
   
 }
